e11052593985c92edb94c6c65c8dc9aa746a3ee0
Choiboin <jdsampaga@gmail.com>
2024-02-03 21:26:37 +0800

    whats a matter baby

--- calorieapp/lib/main.dart
+++ calorieapp/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:calorieapp/pages/Login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.blue[100],
-      ),
+      home: Login(),
     );
   }
 }
--- calorieapp/lib/pages/chatscreen.dart
+++ calorieapp/lib/pages/chatscreen.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class chatscreen extends StatelessWidget {
+  const chatscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
