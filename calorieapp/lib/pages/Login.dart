2e409d686b77f8db4ffaffc13280593de4c9fe2c
Choiboin <jdsampaga@gmail.com>
2024-02-03 22:12:21 +0800

    login page complete

--- calorieapp/lib/pages/Login.dart
+++ calorieapp/lib/pages/Login.dart
@@ -7,18 +7,59 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blue[100],
       appBar: AppBar(title: Text("Login")),
-      body: Center(
-        child: ElevatedButton(
-          child: Text("Login as Guest"),
-          onPressed: () {
-            // navigate to chatscreen
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => chatscreen()), // MaterialPageRoute
-            );
-          },
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Username',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: ElevatedButton(
+                  child: Text("Log In"),
+                  onPressed: () {
+                    // Navigate to chatscreen
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => chatscreen()),
+                    );
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: ElevatedButton(
+                  child: Text("Login as Guest"),
+                  onPressed: () {
+                    // Navigate to chatscreen
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => chatscreen()),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
