9dc0057f9074fed42c65ca68574b7ee57123f02c
Choiboin <jdsampaga@gmail.com>
2024-02-04 00:01:03 +0800

    Login pagev1

--- calorieapp/lib/pages/Login.dart
+++ calorieapp/lib/pages/Login.dart
@@ -1,4 +1,6 @@
+import 'package:calorieapp/pages/Register.dart';
 import 'package:calorieapp/pages/chatscreen.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
@@ -14,22 +16,24 @@ class Login extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              // ... other widgets
               Padding(
                 padding: const EdgeInsets.all(20.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Username',
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                  ),
+                child: Column(
+                  children: [
+                    // Add the text fields here
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Username or Email',
+                      ),
+                    ),
+                    TextField(
+                      obscureText: true, // For password
+                      decoration: InputDecoration(
+                        hintText: 'Password',
+                      ),
+                    ),
+                  ],
                 ),
               ),
               Padding(
@@ -37,12 +41,59 @@ class Login extends StatelessWidget {
                 child: ElevatedButton(
                   child: Text("Log In"),
                   onPressed: () {
-                    // Navigate to chatscreen
+                    // Implement login functionality here
                     Navigator.push(
                       context,
                       MaterialPageRoute(builder: (context) => chatscreen()),
                     );
                   },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'Don\'t have an account? ',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      TextSpan(
+                        text: 'Sign up',
+                        style: TextStyle(color: Colors.blue),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Register()),
+                            );
+                          },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: RichText(
+                  text: TextSpan(
+                    text: 'Forgot password? ',
+                    style: TextStyle(color: Colors.black),
+                    children: [
+                      TextSpan(
+                        text: 'Reset here',
+                        style: TextStyle(color: Colors.blue),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => Login()),
+                            );
+                          },
+                      ),
+                    ],
+                  ),
                 ),
               ),
               Padding(
