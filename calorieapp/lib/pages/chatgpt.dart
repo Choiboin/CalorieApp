6911e411c647acd38ba5a38e7b1d63dc83033f0f
Choiboin <jdsampaga@gmail.com>
2024-02-08 03:22:02 +0800

    chatgpt not workin

--- calorieapp/lib/pages/chatgpt.dart
+++ calorieapp/lib/pages/chatgpt.dart
@@ -1,11 +1,39 @@
-import 'package:flutter/material.dart';
+import 'package:flutter_chatgpt_api/flutter_chatgpt_api.dart';
+import 'package:flutter/material.dart'
 
 class chatgpt extends StatefulWidget {
   const chatgpt({super.key});
 
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
-  }
+  State<chatgpt> createState() => _chatgptState();
 }
+
+ _api = ChatGPTApi(sessionToken: SESSION_TOKEN);
+
+setState(() {
+  _messages.add(
+    ChatMessage(
+      text: _textController.text,
+      chatMessageType: ChatMessageType.user,
+    ),
+  );
+  isLoading = true;
+});  
+
+var newMessage = await _api.sendMessage(
+  input,
+  conversationId: _conversationId,
+  parentMessageId: _parentMessageId,
+);
+
+setState(() {
+  _conversationId = newMessage.conversationId;
+  _parentMessageId = newMessage.messageId;
+  isLoading = false;
+  _messages.add(
+    ChatMessage(
+      text: newMessage.message,
+      chatMessageType: ChatMessageType.bot,
+    ),
+  );
+});
