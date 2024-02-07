7c59f7962f33c2c5abd132085a44bf57cfc7f774
Choiboin <jdsampaga@gmail.com>
2024-02-08 07:57:02 +0800

    scol

--- calorieapp/lib/pages/chatgpt.dart
+++ calorieapp/lib/pages/chatgpt.dart
@@ -1,39 +1,56 @@
 import 'package:flutter_chatgpt_api/flutter_chatgpt_api.dart';
-import 'package:flutter/material.dart'
+import 'package:flutter/material.dart';
 
 class chatgpt extends StatefulWidget {
   const chatgpt({super.key});
 
   @override
-  State<chatgpt> createState() => _chatgptState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[100],
+      appBar: AppBar(
+        title: Text("Chatgpt"),
+      ),
+    );
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
 }
 
- _api = ChatGPTApi(sessionToken: SESSION_TOKEN);
 
-setState(() {
-  _messages.add(
-    ChatMessage(
-      text: _textController.text,
-      chatMessageType: ChatMessageType.user,
-    ),
-  );
-  isLoading = true;
-});  
+//  State<chatgpt> createState() => _chatgptState();
+//}
 
-var newMessage = await _api.sendMessage(
-  input,
-  conversationId: _conversationId,
-  parentMessageId: _parentMessageId,
-);
+// _api = ChatGPTApi(sessionToken: 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);
 
-setState(() {
-  _conversationId = newMessage.conversationId;
-  _parentMessageId = newMessage.messageId;
-  isLoading = false;
-  _messages.add(
-    ChatMessage(
-      text: newMessage.message,
-      chatMessageType: ChatMessageType.bot,
-    ),
-  );
-});
+//setState(() {
+//  _messages.add(
+//    ChatMessage(
+//      text: _textController.text,
+//      chatMessageType: ChatMessageType.user,
+//    ),
+//  );
+//  isLoading = true;
+//});  
+
+//var newMessage = await _api.sendMessage(
+  //input,
+  //conversationId: _conversationId,
+  //parentMessageId: _parentMessageId,
+//);
+
+//setState(() {
+//  _conversationId = newMessage.conversationId;
+//  _parentMessageId = newMessage.messageId;
+//  isLoading = false;
+//  _messages.add(
+//    ChatMessage(
+//      text: newMessage.message,
+//      chatMessageType: ChatMessageType.bot,
+//    ),
+//  );
+//});
