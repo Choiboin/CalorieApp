3eee4bae6a63cf759cddf11255e87aa1e3b94582
Choiboin <jdsampaga@gmail.com>
2024-02-07 22:47:42 +0800

    q

--- calorieapp/lib/data/nlp.dart
+++ calorieapp/lib/data/nlp.dart
@@ -1,4 +1,2 @@
-import 'package:calorieapp/assets/csvs/epi_r.csv';
-import 'package:calorieapp/assets/csvs/nutrients_csvfile.csv'
 import 'package:flutter/material.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
